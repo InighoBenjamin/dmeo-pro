d538088aa44930c850f85218c2fa138c9eaac212
InighoBenjamin <inighobenjamin@gmail.com>
2026-04-17 11:58:29 +0530

    oo

--- Main.java
+++ Main.java
@@ -4,4 +4,8 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("hi inighobenjamin");
     }
+
+public static void println(String message){
+    System.out.println("oooo");
+}
 }
